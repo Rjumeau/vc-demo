ecf0a6ca5ba641cc55c85498fe3566e2099c801b
Romain Jumeau <romain.jumeau@hotmail.fr>
2025-07-13 16:57:03 +0200

    build: add attributes to user

---
+++ db/migrate/20250713145436_add_attributes_to_user.rb
@@ -0,0 +1,8 @@
+class AddAttributesToUser < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :tickets_solved_count, :integer, default: 0
+    add_column :users, :role, :integer, default: 0
+  end
+end
