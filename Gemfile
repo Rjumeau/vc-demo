485b9937e0ff7183a24fcb650d0dae7f52d48827
Romain Jumeau <romain.jumeau@hotmail.fr>
2025-07-14 11:25:39 +0200

    build: add faker factory bot & rspec gem

--- Gemfile
+++ Gemfile
@@ -55,6 +55,10 @@ group :development, :test do
   gem "dotenv-rails"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+
+  gem "faker"
+  gem "factory_bot"
+  gem "rspec"
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, presence: true
+
+  enum role: { teacher: 1, teacher_assistant: 2}
+
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
 end
