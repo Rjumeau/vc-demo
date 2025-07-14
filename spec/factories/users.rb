2757a8fe353d65457df095dae15939052ae033a3
Romain Jumeau <romain.jumeau@hotmail.fr>
2025-07-14 12:04:32 +0200

    feat: update rspec & add user factory & seed

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { "password123" }
+    password_confirmation { "password123" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    tickets_solved_count { rand(0..500) }
+    role { User.roles.except(:batch_manager).keys.sample }
+    image_url { Faker::Avatar.image }
+    batch_count { rand(1..2100) }
+  end
+end
